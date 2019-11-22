2ac24702e4467d3e412d50936f53c707ca5e2f5c
Cu Cong Can <cancc@vitalify.asia>
2019-11-22 14:16:30 +0700

    AnswerProject: change to using ncmb_swift

--- SwiftFirstApp/AppDelegate.swift
+++ SwiftFirstApp/AppDelegate.swift
@@ -3,7 +3,7 @@
 //  SwiftFirstApp
 //
 //  Created by Natsumo Ikeda on 2016/06/22.
-//  Copyright © 2016年 NIFTY Corporation. All rights reserved.
+//  Copyright 2017 FUJITSU CLOUD TECHNOLOGIES LIMITED All Rights Reserved.
 //
 
 import UIKit
@@ -18,9 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let applicationkey = "YOUR_NCMB_APPLICATIONKEY"
     let clientkey      = "YOUR_NCMB_CLIENTKEY"
     
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         //********** SDKの初期化 **********
-        NCMB.setApplicationKey(applicationkey, clientKey: clientkey)
+        NCMB.initialize(applicationKey: applicationkey, clientKey: clientkey)
         
         return true
     }
